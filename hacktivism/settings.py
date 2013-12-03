40ed6baa7f461c01de95e1813e2d82bb0ed7227a
XFJH <soilblack2011@gmail.com>
2013-12-03 05:21:10 +0000

    Update settings.py

--- hacktivism/settings.py
+++ hacktivism/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'hacktivism',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': '105pvsPuDK',
+        'PASSWORD': 'toor',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -37,7 +37,7 @@ TIME_ZONE = 'Asia/Shanghai'
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 #LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'zh-cn'
+LANGUAGE_CODE = 'en-sh'
 
 SITE_ID = 1
 
@@ -89,7 +89,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'm=+r!s28#l-_$z=ux)!#+$^!n*xgm4c%vuzy_ez7xq^uj83a68'
+SECRET_KEY = 'm=+r!s28#l-_$z=ux)0#+$^!n*xgm4c%vuzy_ez7xq^uj83a68'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -108,10 +108,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'hacktivism.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'hacktivism.wsgi.application'
 import os
 TEMPLATE_DIRS = (
     #"C:\\Users\\botgly\\mysite\\mysite\\templates",
